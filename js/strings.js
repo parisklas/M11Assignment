d80d225ac781a40068cd2c0a465e52f8860552c6
parisklas <parisklas@gmail.com>
2024-01-06 12:34:02 -0500

    Completed strings.js

--- js/strings.js
+++ js/strings.js
@@ -1,19 +1,36 @@
 //STEP 1
-
+  // let nameLength = window.prompt('What is your name?');
+  // alert('Your name has ' + nameLength.length + ' characters.');
 //STEP 2
-
+  // let nameLength = window.prompt('What is your name?');
+  // let letterAt = window.prompt('Enter a numeric value to find the corresponding letter position');
+  // alert(`The letter at index ${letterAt} is: ` + nameLength.charAt(letterAt));
 //STEP 3
-
+  // let firstName = window.prompt('What is your first name?');
+  // let lastName = window.prompt('What is your last name?');
+  // alert("Your name is: " + firstName + " " + lastName);
 //STEP 4
-
+  // let text = "The quick brown fox jumps over the lazy dog";
+  // alert('The index of the word fox is ' + text.indexOf('fox'));
 //STEP 5
-
+  // let text = "The quick brown fox jumps over the lazy fox";
+  // alert('The index of the word fox is ' + text.lastIndexOf('fox'));
 //STEP 6
-
+  // let text = "The quick brown fox jumps over the lazy dog";
+  // let fullName = window.prompt('What is your full name?');
+  // alert(text.replace('the lazy dog', `${fullName}`));
 //STEP 7
-
+  // let text = "The quick brown fox jumps over the lazy dog";
+  // let word = window.prompt('please type in a word in the text to search');
+  // alert(text.search(word));
 //STEP 8
-
+  // let old_string = "The quick brown fox jumps over the lazy dog";
+  // console.log(old_string.indexOf('the lazy dog'));
+  // let new_string = old_string.slice(31, 43);
+  // alert(new_string.toUpperCase())
 //STEP 9
-
+  // let text = " THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG          ";
+  // alert(text.trim().toLowerCase());
 //STEP 10
+  // let text = "the quick brown fox jumps over the lazy dog";
+  // alert(text[0].toUpperCase() + text.slice(1));
