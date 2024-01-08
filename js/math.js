eaa972b051f9721f1271873a44dd7e9d7db4adb6
parisklas <parisklas@gmail.com>
2024-01-08 16:10:10 -0500

    Completed Math section

--- js/math.js
+++ js/math.js
@@ -1,9 +1,19 @@
 //STEP 1
-
+  // let num = prompt('Please enter a number');
+  // console.log('The absolute value is ' + Math.abs(num));
 //STEP 2
-
+  // let num = prompt('Please enter a decimal number');
+  // console.log('The nearest whole number (rounding up) is ' + Math.ceil(num));
 //STEP 3
-
+  // let num = prompt('Please enter a decimal number');
+  // console.log('The nearest whole number (rounding down) is ' + Math.floor(num));
 //STEP 4
-
+  // let numbers = prompt('Please enter 5 numbers in the following format: x,x,x,x,x');
+  // let array = numbers.split(','); // turning string into array
+  // let max = Math.max(...array); // spread operator
+  // let min = Math.min(...array); // spread operator
+  // console.log('The number with the highest value is ' + max);
+  // console.log('The number with the lowest value is ' + min);
 //STEP 5
+  // let num = prompt('Please enter a number');
+  // console.log(`The square root of ${num} is ` + Math.sqrt(num));
